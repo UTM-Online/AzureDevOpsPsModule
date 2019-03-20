c1795e6bf11e0ad809247c6b6aa89bd972a7dffd
Josh Irwin <joirwi@microsoft.com>
2019-03-20 09:35:20 -0700

    Adding a base class to be used with all cmdlets that make REST calls to Azure Dev Ops

---
+++ AzureDevOpsMgmt/AzureDevOpsMgmt.Helpers/Cmdlets/PSCmdletPrivateBase.cs
@@ -0,0 +1,38 @@
+﻿namespace AzureDevOpsMgmt.Cmdlets
+{
+    using System;
+    using System.Management.Automation;
+    using System.Reflection;
+
+    using AzureDevOpsMgmt.Helpers;
+
+    using RestSharp;
+
+    public abstract class PSCmdletPrivateBase : PSCmdlet
+    {
+        /// <summary>
+        /// The client
+        /// </summary>
+        protected RestClient client;
+
+        protected override void BeginProcessing()
+        {
+            this.client = this.GetRestClient();
+            AppDomain.CurrentDomain.AssemblyResolve += this.CurrentDomain_BindingRedirect;
+        }
+
+        private Assembly CurrentDomain_BindingRedirect(object sender, ResolveEventArgs args)
+        {
+            var name = new AssemblyName(args.Name);
+
+            switch (name.Name)
+            {
+                case "Newtonsoft.Json":
+                    return typeof(Newtonsoft.Json.JsonSerializer).Assembly;
+
+                default:
+                    return null;
+            }
+        }
+    }
+}
