5c0f0e49bfc96cabb36626b573fa25ca2109b7fe
Josh Irwin <joshua@irwin.ws>
2019-03-26 11:28:55 -0700

    Adding some more infrastructure to make error handling and reporting more uniform and efficient.

---
+++ AzureDevOpsMgmt/AzureDevOpsMgmt.Helpers/Models/DevOpsModelTarget.cs
@@ -0,0 +1,40 @@
+﻿// ***********************************************************************
+// Assembly         : AzureDevOpsMgmt.Core
+// Author           : joirwi
+// Created          : 03-26-2019
+//
+// Last Modified By : joirwi
+// Last Modified On : 03-26-2019
+// ***********************************************************************
+// <copyright file="DevOpsModelTarget.cs" company="Microsoft">
+//     Copyright ©  2019
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+namespace AzureDevOpsMgmt.Models
+{
+    /// <summary>
+    /// An enumeration of possible resource types the module can target in Azure Dev Ops
+    /// This is primary used for error reporting purposes in the generation of the Standard Error ID.
+    /// </summary>
+    public enum DevOpsModelTarget
+    {
+        /// <summary>
+        /// Work Item Resources
+        /// </summary>
+        WorkItem,
+
+        /// <summary>
+        /// Build Resources
+        /// </summary>
+        Build,
+
+        /// <summary>
+        /// Release Resources
+        /// </summary>
+        Release,
+
+        /// <summary>  Indicates that the error took place during internal processing of an operation.</summary>
+        InternalOperation
+    }
+}
