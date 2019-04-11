b840014e6e27b9d270ce900f8cd590ffba5c1854
Josh Irwin <joshua@irwin.ws>
2019-04-11 09:42:01 -0700

    Fixing a bug in the SetStartUpAccount cmdlet that caused it to write the change to memory but not to disk.

--- AzureDevOpsMgmt/AzureDevOpsMgmt.Core/Models/UserConfiguration.cs
+++ AzureDevOpsMgmt/AzureDevOpsMgmt.Core/Models/UserConfiguration.cs
@@ -1,9 +1,17 @@
 ﻿namespace AzureDevOpsMgmt.Models
 {
+    using AzureDevOpsMgmt.Helpers;
+    using AzureDevOpsMgmt.Resources;
+
     public class UserConfiguration
     {
         public string DefaultAccount { get; set; }
 
         public string DefaultProject { get; set; }
+
+        public void Save()
+        {
+            FileHelpers.WriteFileJson(FileNames.UserData, this);
+        }
     }
 }
