07b0d652f3c4b5dfaab30800e291eca15b2ae644
Josh Irwin <joshua@irwin.ws>
2019-04-16 16:11:44 -0700

    Adding the first query that will be used as part of moving remaining work items.  Started with some refactoring to accomidate the requirements for the Move-RemainingWorkToNextSprint cmdlet.

---
+++ AzureDevOpsMgmt/AzureDevOpsMgmt.Core/Cmdlets/Assisstants/MoveRemainingWorkToNextSprint.cs
@@ -0,0 +1,41 @@
+﻿namespace AzureDevOpsMgmt.Cmdlets.Assisstants
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Management.Automation;
+    using Helpers;
+    using Microsoft.TeamFoundation.Work.WebApi;
+    using Models;
+    using Resources;
+    using RestSharp;
+
+    public class MoveRemainingWorkToNextSprint : ApiCmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public MoveOption MoveOption { get; set; }
+
+        [Parameter]
+        public string SourceSprint { get; set; }
+
+        [Parameter]
+        public string DestinationSprint { get; set; }
+
+        protected override string OverrideApiPath => $"/{Uri.EscapeUriString(AzureDevOpsConfiguration.Config.CurrentConnection.ProjectName)}/_apis";
+
+        protected override void BeginProcessingCmdlet()
+        {
+            var sourceSprintVerificationRequest = new RestRequest("/work/teamsettings/iterations");
+
+            var sourceSprintVerificationResponse = this.client.Get<List<TeamSettingsIteration>>(sourceSprintVerificationRequest);
+
+            if (sourceSprintVerificationResponse.IsSuccessful)
+            {
+
+            }
+            else
+            {
+                this.WriteError(sourceSprintVerificationResponse.ErrorException, this.BuildStandardErrorId(DevOpsModelTarget.WorkItem), ErrorCategory.InvalidResult, this);
+            }
+        }
+    }
+}
