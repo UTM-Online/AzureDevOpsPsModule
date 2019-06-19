33e95c817ec0bb81463d9454816f7df0a027b6de
Joshua Irwin <joshua@irwin.ws>
2019-06-18 19:54:20 -0700

    Checking in some more work

--- AzureDevOpsMgmt/AzureDevOpsMgmt.Core/Resources/StaticStrings.Designer.cs
+++ AzureDevOpsMgmt/AzureDevOpsMgmt.Core/Resources/StaticStrings.Designer.cs
@@ -19,7 +19,7 @@ namespace AzureDevOpsMgmt.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class StaticStrings {
@@ -66,6 +66,15 @@ namespace AzureDevOpsMgmt.Resources {
         internal static string ApplicationName {
             get {
                 return ResourceManager.GetString("ApplicationName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to The default string.
+        /// </summary>
+        internal static string Default {
+            get {
+                return ResourceManager.GetString("Default", resourceCulture);
             }
         }
         
