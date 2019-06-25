c8866920bc675627468e047ec31c9acc9efed38d
Josh Irwin <josh.irwin@microsoft.com>
2019-06-24 17:34:05 -0700

    Adding a unit test to validate the functionality of the DI code being added to the module.

---
+++ AzureDevOpsMgmt/AzureDevOpsMgmt.CoreTests/Cmdlets/BaseCmdletTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AzureDevOpsMgmt.CoreTests.Cmdlets
+{
+    using System.Linq;
+    using System.Management.Automation;
+
+    using AzureDevOpsMgmt.CoreTests.Models;
+
+    using RestSharp;
+
+    [TestClass]
+    public class BaseCmdletTests
+    {
+        [TestMethod]
+        public void VerifyDiFunctionTest()
+        {
+            Cmdlet testCmdlet = new TestCmdlet();
+
+            var results = testCmdlet.Invoke<IRestClient>()?.First();
+
+            Assert.IsNotNull(results);
+        }
+    }
+}
---
+++ AzureDevOpsMgmt/AzureDevOpsMgmt.CoreTests/Models/TestCmdlet.cs
@@ -0,0 +1,12 @@
+﻿namespace AzureDevOpsMgmt.CoreTests.Models
+{
+    using AzureDevOpsMgmt.Cmdlets;
+
+    public class TestCmdlet : ApiCmdlet
+    {
+        protected override void ProcessCmdletRecord()
+        {
+            this.WriteObject(this.client);
+        }
+    }
+}
