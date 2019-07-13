039564fbc988e7ceb8ed9cec144f75096947da50
Joshua Irwin <joshua@irwin.ws>
2019-07-13 13:35:10 -0700

    Checking in framework for GetCurrentIteration

---
+++ AzureDevOpsMgmt/AzureDevOpsMgmt.Core/Cmdlets/WorkItems/GetCurrentIteration.cs
@@ -0,0 +1,10 @@
+﻿namespace AzureDevOpsMgmt.Cmdlets.WorkItems
+{
+    using RestSharp;
+
+    public class GetCurrentIteration : ApiCmdlet
+    {
+        [ShouldInject("AdoTeamsApi")]
+        protected override IRestClient Client { get; set; }
+    }
+}
