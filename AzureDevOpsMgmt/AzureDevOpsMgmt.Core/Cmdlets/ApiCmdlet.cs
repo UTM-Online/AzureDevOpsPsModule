d2089325363ef5f7032cc30bbc793492fc979f4b
Josh Irwin <josh.irwin@microsoft.com>
2019-07-15 15:33:04 -0700

    Resolving issue described in bug #171

--- AzureDevOpsMgmt/AzureDevOpsMgmt.Core/Cmdlets/ApiCmdlet.cs
+++ AzureDevOpsMgmt/AzureDevOpsMgmt.Core/Cmdlets/ApiCmdlet.cs
@@ -85,37 +85,158 @@
             {
                 if (useExceptionTypeChecking)
                 {
-                    switch (response.ErrorException)
-                    {
-                        case JsonSerializationException jse:
-                            {
-                                if (jse.Message.StartsWith("Cannot deserialize the current JSON object"))
-                                {
-                                    this.WriteError(response.ErrorException, this.BuildStandardErrorId(DevOpsModelTarget.Build, "ObjectDeserializationFailed"), ErrorCategory.ReadError, onErrorTargetObject);
-                                    break;
-                                }
-                                else
-                                {
-                                    goto default;
-                                }
-                            }
-                        default:
-                            {
-                                this.WriteErrorInternal(response, onErrorTarget, onErrorCategory, onErrorTargetObject, onErrorReason);
-
-                                break;
-                            }
-                    }
+                    this.ProcessErrorResponse(response, onErrorTarget, onErrorCategory, onErrorTargetObject, onErrorReason);
                 }
                 else
                 {
-                    this.WriteErrorInternal(response, onErrorTarget, onErrorCategory, onErrorTargetObject, onErrorReason);
+                    this.WriteErrorInternal(
+                        response,
+                        onErrorTarget,
+                        onErrorCategory,
+                        onErrorTargetObject,
+                        onErrorReason);
                 }
+            }
+        }
+
+        protected void ProcessErrorResponse(
+            IRestResponse response,
+            DevOpsModelTarget onErrorTarget,
+            ErrorCategory onErrorCategory,
+            object onErrorTargetObject,
+            string onErrorReason = null)
+        {
+            switch (response.ErrorException)
+            {
+                case JsonSerializationException jse:
+                    {
+                        if (jse.Message.StartsWith("Cannot deserialize the current JSON object"))
+                        {
+                            this.WriteError(
+                                response.ErrorException,
+                                this.BuildStandardErrorId(onErrorTarget, "ObjectDeserializationFailed"),
+                                ErrorCategory.ReadError,
+                                onErrorTargetObject);
+                            break;
+                        }
+
+                        goto default;
+                    }
+
+                case JsonReaderException jre:
+                    {
+                        if (jre.Message.StartsWith("Unexpected character encountered while parsing value:")
+                            && response.Content.Contains("Access Denied: The Personal Access Token used has expired."))
+                        {
+                            this.WriteError(
+                                response.ErrorException,
+                                this.BuildStandardErrorId(onErrorTarget, "Your PAT Token has expired!!"),
+                                ErrorCategory.AuthenticationError,
+                                onErrorTargetObject);
+                            break;
+                        }
+
+                        goto default;
+                    }
+
+                default:
+                    {
+                        this.WriteErrorInternal(response, onErrorTarget, onErrorCategory, onErrorTargetObject, onErrorReason);
+
+                        break;
+                    }
+            }
+        }
+
+        /// <summary>
+        ///     Begins the cmdlet processing.
+        /// </summary>
+        protected override sealed void BeginCmdletProcessing()
+        {
+            if (!AzureDevOpsConfiguration.Config.ReadyForCommands)
+            {
+                // ReSharper disable once ExceptionNotDocumented
+                this.ThrowTerminatingError(
+                    new ErrorRecord(
+                        new InvalidOperationException(
+                            "Account context has not been set.  Please run \"Set-AzureDevOpsAccountContext\" before continuing."),
+                        "AzureDevOps.Cmdlet.Auth.AccountContextNotSetException",
+                        ErrorCategory.AuthenticationError,
+                        this));
+            }
+
+            if (this.OverrideApiPath != null)
+            {
+                var currentAccount = AzureDevOpsConfiguration.Config.CurrentConnection;
+                var escapedProjectString = Uri.EscapeUriString(currentAccount.ProjectName);
+
+                this.Client.BaseUrl = new Uri(
+                    $"{currentAccount.Account.BaseUrl}/{escapedProjectString}{this.OverrideApiPath}");
+            }
+
+            AppDomain.CurrentDomain.AssemblyResolve += this.CurrentDomain_BindingRedirect;
+
+            this.BeginProcessingCmdlet();
+        }
+
+        /// <summary>
+        ///     Begins the processing cmdlet.
+        /// </summary>
+        protected virtual void BeginProcessingCmdlet()
+        {
+        }
+
+        /// <summary>
+        ///     Handles the BindingRedirect event of the CurrentDomain control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="args">The <see cref="ResolveEventArgs" /> instance containing the event data.</param>
+        /// <returns>An Assembly.</returns>
+        private Assembly CurrentDomain_BindingRedirect(object sender, ResolveEventArgs args)
+        {
+            var name = new AssemblyName(args.Name);
+
+            switch (name.Name)
+            {
+                case "Newtonsoft.Json":
+                    return typeof(JsonSerializer).Assembly;
+
+                default:
+                    return null;
             }
         }
 
         private void WriteErrorInternal<T>(
             IRestResponse<T> response,
+            DevOpsModelTarget onErrorTarget,
+            ErrorCategory onErrorCategory,
+            object onErrorTargetObject,
+            string onErrorReason)
+        {
+            string errorId;
+
+            if (string.IsNullOrWhiteSpace(onErrorReason))
+            {
+                errorId = this.BuildStandardErrorId(onErrorTarget);
+            }
+            else
+            {
+                errorId = this.BuildStandardErrorId(onErrorTarget, onErrorReason);
+            }
+
+            this.WriteError(response.ErrorException, errorId, onErrorCategory, onErrorTargetObject);
+        }
+
+        /// <summary>
+        ///     Writes the error internal.
+        /// </summary>
+        /// <param name="response">The response.</param>
+        /// <param name="onErrorTarget">The on error target.</param>
+        /// <param name="onErrorCategory">The on error category.</param>
+        /// <param name="onErrorTargetObject">The on error target object.</param>
+        /// <param name="onErrorReason">The on error reason.</param>
+        private void WriteErrorInternal(
+            IRestResponse response,
             DevOpsModelTarget onErrorTarget,
             ErrorCategory onErrorCategory,
             object onErrorTargetObject,
