5ea3d630895c461ce9d5747d8df47c5abd00e726
Joshua Irwin <joshua@irwin.ws>
2020-08-10 19:01:04 -0700

    Checking in a little work

---
+++ AzureDevOpsMgmt/AzureDevOpsMgmt.Core/Cmdlets/InvokeAdoRestApi.cs
@@ -0,0 +1,14 @@
+﻿namespace AzureDevOpsMgmt.Cmdlets
+{
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class InvokeAdoRestApi : ApiCmdlet
+    {
+        public string ResourcePath { get; set; }
+
+        public List<KeyValuePair<string,string>> QueryParameters { get; set; }
+
+        
+    }
+}
